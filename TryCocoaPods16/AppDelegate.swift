dae0bb496452a6145b57e8e8b42c45322ebe36fe
Sergei Shirokov <sshirokov@malwarebytes.com>
2018-09-08 11:59:57 +0300

    Initial Commit

---
+++ TryCocoaPods16/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TryCocoaPods16
+//
+//  Created by Sergei Shirokov on 9/8/18.
+//  Copyright © 2018 Foo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TryCocoaPods16/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TryCocoaPods16
+//
+//  Created by Sergei Shirokov on 9/8/18.
+//  Copyright © 2018 Foo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
